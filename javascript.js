e25617f8dcd98c95dd280682802a363311891b3b
Jason <123558105+JChow999@users.noreply.github.com>
2023-10-26 10:57:53 -0400

    Adjust javascript and index Add id to index for playerscore and computer score Change javascript score to update player and computer scores

--- javascript.js
+++ javascript.js
@@ -2,52 +2,46 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
+const playerScoreText = document.querySelector("#yourScore");
+const compScoreText = document.querySelector("#compScore");
+
 let playerScore = 0;
 let computerScore = 0;
 
-function game() {
-    // for (let step = 0; step < 5; step++) {
-        const choices = ['rock', 'paper', 'scissors'];
-        let playerSelection = ""
-        do {
-            rock.addEventListener('click', () => console.log("rock"));
-            paper.addEventListener('click', () => playerSelection='paper');
-            scissors.addEventListener('click', () => playerSelection='scissors');
-            console.log(playerSelection)
-            }
-        while (!choices.includes(playerSelection));
-        let computerSelection = getComputerChoice();
-
-        console.log(playRound(playerSelection, computerSelection));
-        playerSelection = ""
-    // }
-}
+rock.addEventListener('click', () => playRound('rock', getComputerChoice()));
+paper.addEventListener('click', () => playRound('paper', getComputerChoice()));
+scissors.addEventListener('click', () => playRound('scissors', getComputerChoice()));
 
 function playRound(playerSelection, computerSelection) {
-    const choices = {
-        rock: 0, 
-        paper: 1, 
-        scissors: 2
-    };
-
     // These if statements are to determine the winner the game to announce the results
-    if (choices.hasOwnProperty(playerSelection)) {
-        if (playerSelection == computerSelection) {
-            return "It's a Tie!"
-        } else if (choices[playerSelection] == 0 && choices[computerSelection] == 2) {
-            return `You Win! ${titleCase(playerSelection)} beats ${titleCase(computerSelection)}!`
-        } else if (choices[playerSelection] == 2 && choices[computerSelection] == 0) {
-            return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelection)}!`
-        } else if (choices[playerSelection] < choices[computerSelection]) {
-            return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelection)}!`
-        } else if (choices[playerSelection] > choices[computerSelection]) {
-            return `You Win! ${titleCase(playerSelection)} beats ${titleCase(computerSelection)}!`
-        } else {
-            return ("Something bad happened.")
-        }
-    } else {
-        return "Error, invalid choice."
+    choices = {
+        rock: 0,
+        paper: 1,
+        scissors: 2,
     }
+    console.log(playerScore, computerScore)
+    playerScoreText.textContent = `Your Score: ${playerScore}`;
+    compScoreText.textContent = `Your Score: ${computerScore}`;
+
+    if (playerSelection == computerSelection) {
+        return "It's a Tie!"
+    } else if (choices[playerSelection] == 0 && choices[computerSelection] == 2) {
+        playerScore++
+        return `You Win! ${titleCase(playerSelection)} beats ${titleCase(computerSelection)}!`
+    } else if (choices[playerSelection] == 2 && choices[computerSelection] == 0) {
+        computerScore++
+        return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelection)}!`
+    } else if (choices[playerSelection] < choices[computerSelection]) {
+        computerScore++
+        return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelection)}!`
+    } else if (choices[playerSelection] > choices[computerSelection]) {
+        playerScore++
+        return `You Win! ${titleCase(playerSelection)} beats ${titleCase(computerSelection)}!` 
+    } else {
+        return ("Something bad happened.")
+    }
+
+    
   }
 
   // Function to capitalize the first letter of a word to reduce code length in template literals
@@ -66,6 +60,15 @@ function getComputerChoice() {
     return compChoice
 }
 
-  
-game();
+// function game() {
+//     let playerSelection = ""
+
+//     let computerSelection = getComputerChoice();
+
+    
+    
+//     console.log(playerScore, computerScore)
+// }
+
+
   
